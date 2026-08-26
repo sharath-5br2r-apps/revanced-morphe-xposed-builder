db54a5489a28c04396db22e836ed019948f53bf7
Sharath <sharathsukash@gmail.com>
2026-08-26 20:07:01 +0530

    refactor(ci): increase batch parts from 10 to 15 (~9 apps per batch job)

--- .github/scripts/ci_merge_patch_tomls.py
+++ .github/scripts/ci_merge_patch_tomls.py
@@ -47,7 +47,7 @@ def main():
             all_tables.append((patchset_name, header, content))
 
     total_apps = len(all_tables)
-    NUM_OUTPUT_FILES = 10
+    NUM_OUTPUT_FILES = 15
     apps_per_file = math.ceil(total_apps / NUM_OUTPUT_FILES)
 
     matrix_jobs = []
